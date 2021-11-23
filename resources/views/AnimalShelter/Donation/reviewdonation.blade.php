1df97c0a46f606f87791e3138a22d5b5f7264d48
bellitachristian <bellitachristian78u@gmail.com>
2021-11-23 21:14:33 +0800

    bug fixed

--- resources/views/AnimalShelter/Donation/reviewdonation.blade.php
+++ resources/views/AnimalShelter/Donation/reviewdonation.blade.php
@@ -10,7 +10,7 @@ View Donation Proof
                 <a href="{{route('view.donation')}}"><button type="button" class="btn btn-danger">Back</button></a>
             </div>
             <div class="card-body">
-            <img src="{{asset('phpcode/validid/'.$images->donor_photo)}}" width="100%" height="600px" />
+            <img src="{{asset('phpcode/donation/'.$images->donor_photo)}}" width="100%" height="600px" />
             </div>
         </div>
     </div>
