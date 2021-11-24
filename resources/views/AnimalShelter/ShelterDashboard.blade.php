d9657f200f874c4034994826e0eab325b89dcad8
bellitachristian <bellitachristian78u@gmail.com>
2021-11-24 20:04:22 +0800

    added

--- resources/views/AnimalShelter/ShelterDashboard.blade.php
+++ resources/views/AnimalShelter/ShelterDashboard.blade.php
@@ -51,11 +51,12 @@ Animal Shelter Dashboard
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-info text-uppercase mb-1"><h5 style="font-weight:bold">Adoption Requests</h5>
-                    {{$countrequest}}
                     </div>
                     <div class="row no-gutters align-items-center">
                         <div class="col-auto">
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                {{$countrequest}}
+                            </div>
                         </div>
                     </div>
                 </div>
