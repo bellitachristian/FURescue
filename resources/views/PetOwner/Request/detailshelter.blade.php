46f6dae4e92eed873c7450fff4e378285430c53f
bellitachristian <bellitachristian78u@gmail.com>
2021-11-25 09:56:22 +0800

    change

--- resources/views/PetOwner/Request/detailshelter.blade.php
+++ resources/views/PetOwner/Request/detailshelter.blade.php
@@ -13,30 +13,24 @@ Shelter Details
                     <div class="card-group-row" style="display:flex">
                             <div class="col-sm"> 
                                 <div style="text-align:center">
-                                    <img src="{{asset('uploads/animal-shelter/profile/'.$shelter->profile)}}" style="border-radius:50%;"height="209" width="270" alt="">
+                                    <img src="{{asset('uploads/animal-shelter/profile/'.$shelter->profile)}}" style="border-radius:50%;"height="100" width="130" alt="">
                                 </div>       
                                 <label style="font-height:1">Name of Shelter</label>                        
                                 <input style="margin-bottom:1%" type="text"  value="{{$shelter->shelter_name}}" readOnly class="form-control form-control-sm">
                                 <label >Address</label>
                                 <input style="margin-bottom:5%" type="text" value="{{$shelter->address}}" readOnly class="form-control form-control-sm">
-                            </div>  
-                            <div class="col-sm">
                                 <h5 style="color:black; margin-top:5%; margin-bottom:4%">Shelter Information</h5>
                                 <label >Animal Shelter Contact Person</label>
                                 <input style="margin-bottom:1%" type="text" value ="{{$shelter->founder_name}}" readOnly  class="form-control form-control-sm">
                                 <label >Contact Number</label>
                                 <input style="margin-bottom:1%" type="text" value ="{{$shelter->contact}}" readOnly  class="form-control form-control-sm">
                                 <label >Email</label>
-                                <input style="margin-bottom:5%" type="email" value ="{{$shelter->email}}"readOnly class="form-control form-control-sm">
-                                
-                            </div>
+                                <input style="margin-bottom:5%" type="email" value ="{{$shelter->email}}"readOnly class="form-control form-control-sm">  
+                            </div>  
                             <div class="col-sm">
-                                <h5 style="color:black; margin-top:5%; margin-bottom:4%">Account Information</h5>
-                                <label >Gcash Number</label>
-                                <input  style="margin-bottom:1%" type="number" value ="{{$shelter->g_cash}}" readOnly class="form-control form-control-sm">
-                                <label >Paypal Number</label>
-                                <input style="margin-bottom:4%" type="number" value ="{{$shelter->pay_pal}}" readOnly class="form-control form-control-sm">           
-                            <div>
+                                <h5 style="color:black; margin-top:5%; margin-bottom:4%">Pets Catered</h5>
+
+                            </div>
                         </div>   
                     </div>        
                 </div>                                  
