8f60df27afff9fcde6febbd82e59e766a57da443
bellitachristian <bellitachristian78u@gmail.com>
2021-11-25 16:35:34 +0800

    1

--- resources/views/PetOwner/Request/animal.blade.php
+++ resources/views/PetOwner/Request/animal.blade.php
@@ -17,9 +17,10 @@ Animals to be Adopted
                 </form>
             </div>
             <div class="card-body">
-                <table id="datatable" class="table table-light table-hover">
+                <table id="example" class="display select" cellspacing="0" width="100%">
                     <thead>
                         <tr>
+                            <th><input type="checkbox" name="select_all" value="1" id="example-select-all">
                             <th>ID</th>
                             <th>Animal Photo</th>
                             <th>Name</th>
@@ -58,47 +59,67 @@ Animals to be Adopted
 @push('js')
 <script src="https://cdn.datatables.net/1.11.2/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript">
-       $(document).ready(function() {
-    let myTable = $('#datatable').DataTable({
-        columnDefs: [{
-            orderable: false,
-            className: 'select-checkbox',
-            targets: 0,
-        }],
-        select: {
-            style: 'os', // 'single', 'multi', 'os', 'multi+shift'
-            selector: 'td:first-child',
-        },
-        order: [
-            [1, 'asc'],
-        ],
-    });
+     $(document).ready(function (){
+   var table = $('#example').DataTable({
+      'ajax': {
+         'url': '/lab/articles/jquery-datatables-how-to-add-a-checkbox-column/ids-arrays.txt'
+      },
+      'columnDefs': [{
+         'targets': 0,
+         'searchable': false,
+         'orderable': false,
+         'className': 'dt-body-center',
+         'render': function (data, type, full, meta){
+             return '<input type="checkbox" name="id[]" value="' + $('<div/>').text(data).html() + '">';
+         }
+      }],
+      'order': [[1, 'asc']]
+   });
 
-       myTable.on('select deselect draw', function () {
-        var all = myTable.rows({ search: 'applied' }).count(); // get total count of rows
-        var selectedRows = myTable.rows({ selected: true, search: 'applied' }).count(); // get total count of selected rows
+   // Handle click on "Select all" control
+   $('#example-select-all').on('click', function(){
+      // Get all rows with search applied
+      var rows = table.rows({ 'search': 'applied' }).nodes();
+      // Check/uncheck checkboxes for all rows in the table
+      $('input[type="checkbox"]', rows).prop('checked', this.checked);
+   });
 
-        if (selectedRows < all) {
-            $('#datatable i').attr('class', 'fa fa-square-o');
-        } else {
-            $('#MyTableCheckAllButton i').attr('class', 'fa fa-check-square-o');
-        }
+   // Handle click on checkbox to set state of "Select all" control
+   $('#example tbody').on('change', 'input[type="checkbox"]', function(){
+      // If checkbox is not checked
+      if(!this.checked){
+         var el = $('#example-select-all').get(0);
+         // If "Select all" control is checked and has 'indeterminate' property
+         if(el && el.checked && ('indeterminate' in el)){
+            // Set visual state of "Select all" control
+            // as 'indeterminate'
+            el.indeterminate = true;
+         }
+      }
+   });
 
-    });
+   // Handle form submission event
+   $('#frm-example').on('submit', function(e){
+      var form = this;
 
-    $('#MyTableCheckAllButton').click(function () {
-        var all = myTable.rows({ search: 'applied' }).count(); // get total count of rows
-        var selectedRows = myTable.rows({ selected: true, search: 'applied' }).count(); // get total count of selected rows
+      // Iterate over all checkboxes in the table
+      table.$('input[type="checkbox"]').each(function(){
+         // If checkbox doesn't exist in DOM
+         if(!$.contains(document, this)){
+            // If checkbox is checked
+            if(this.checked){
+               // Create a hidden element
+               $(form).append(
+                  $('<input>')
+                     .attr('type', 'hidden')
+                     .attr('name', this.name)
+                     .val(this.value)
+               );
+            }
+         }
+      });
+   });
 
-
-        if (selectedRows < all) {
-            //Added search applied in case user wants the search items will be selected
-            myTable.rows({ search: 'applied' }).deselect();
-            myTable.rows({ search: 'applied' }).select();
-        } else {
-            myTable.rows({ search: 'applied' }).deselect();
-        }
-    });
 });
     </script>
 @endpush
