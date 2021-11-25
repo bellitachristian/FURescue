310a3b1c207d912886445fcab68bae2ba36eae50
bellitachristian <bellitachristian78u@gmail.com>
2021-11-25 16:59:16 +0800

    1

--- resources/views/PetOwner/Request/animal.blade.php
+++ resources/views/PetOwner/Request/animal.blade.php
@@ -3,7 +3,7 @@
 Animals to be Adopted
 @endsection
 @push("css")
-<link type="text/css" href="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/css/dataTables.checkboxes.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.2/css/jquery.dataTables.min.css">   
 @endpush
 @section("content")
 <div class="row">
@@ -11,16 +11,13 @@ Animals to be Adopted
         <div class="card shadow mb-4">
             <div class="card-header">
                 <form id="myForm" action="POST">
-                <button style="border: none; background: transparent; font-size: 14px;" id="MyTableCheckAllButton">
-                <i class="far fa-square"></i>  
-                </button>                
+                    <a href="#"><button type="button" class="btn btn-success">Submit Selected</button></a>
                 </form>
             </div>
             <div class="card-body">
-                <table id="example" class="display select" cellspacing="0" width="100%">
+                <table id="datatable" class="table table-light table-hover">
                     <thead>
                         <tr>
-                            <th><input type="checkbox" name="select_all" value="1" id="example-select-all">
                             <th>ID</th>
                             <th>Animal Photo</th>
                             <th>Name</th>
@@ -35,7 +32,6 @@ Animals to be Adopted
                         <tr>
                             <td>{{$animal->id}}</td>
                             <td>
-                                <img src="{{asset('uploads/animals/'.$animal->animal_image)}}" width="70px" height="70px" alt="">
                             </td>
                             <td>{{$animal->name}}</td>
                             <td>{{$animal->breed}}</td>
@@ -57,41 +53,10 @@ Animals to be Adopted
 </div>
 @endsection
 @push('js')
-<script type="text/javascript" src="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/js/dataTables.checkboxes.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.2/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript">
-     $(document).ready(function (){
-        var table = $('#example').DataTable({
-            'ajax': '/lab/jquery-datatables-checkboxes/ids-arrays.txt',
-            'columnDefs': [
-                {
-                    'targets': 0,
-                    'checkboxes': {
-                    'selectRow': true
-                    }
-                }
-            ],
-            'select': {
-                'style': 'multi'
-            },
-            'order': [[1, 'asc']]
-        });
-
-        // Handle form submission event
-        $('#frm-example').on('submit', function(e){
-            var form = this;
-
-            var rows_selected = table.column(0).checkboxes.selected();
-
-            // Iterate over all selected checkboxes
-            $.each(rows_selected, function(index, rowId){
-                // Create a hidden element
-                $(form).append(
-                    $('<input>')
-                        .attr('type', 'hidden')
-                        .attr('name', 'id[]')
-                        .val(rowId)
-                );
-            });
+        $(document).ready(function(){
+          var table = $('#datatable').DataTable({
         });
     });
     </script>
