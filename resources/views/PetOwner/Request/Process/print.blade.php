f2a964d43d3c2007907bf8fdf4a402ca8faef71b
bellitachristian <bellitachristian78u@gmail.com>
2021-12-06 10:47:07 +0800

    1

--- resources/views/PetOwner/Request/Process/print.blade.php
+++ resources/views/PetOwner/Request/Process/print.blade.php
@@ -5,7 +5,7 @@ Adoption Slip
 @push("css")
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="{{url('css/print.css')}}">
+<link rel="stylesheet" href="{{url('/css/print.css')}}">
 @endpush
 @section("content")             
 <div class="slip-content">
