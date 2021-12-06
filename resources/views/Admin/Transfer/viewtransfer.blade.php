bbd40148d9d32606baa6d97b52cecb58137afc86
bellitachristian <bellitachristian78u@gmail.com>
2021-12-06 22:32:45 +0800

    1

--- resources/views/Admin/Transfer/viewtransfer.blade.php
+++ resources/views/Admin/Transfer/viewtransfer.blade.php
@@ -36,7 +36,7 @@ Transfer Adoption Fee
                             <td>
                                 <a href=""><button class="btn btn-success">Transfer</button></a>
                             </td>
-                            @elseif(($money->usertype == 3)
+                            @elseif($money->usertype == 3)
                             <td>{{$money->usertype->usertype}}</td>
                                     <td>{{$money->petowner->fname}} {{$money->petowner->lname}}</td>
                                     <td>{{$money->petowner->email}}</td>
