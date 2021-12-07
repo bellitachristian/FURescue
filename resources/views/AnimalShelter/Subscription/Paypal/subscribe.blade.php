87319b5e5ff9a00eed576a9fc8d53ad5111e77df
bellitachristian <bellitachristian78u@gmail.com>
2021-12-07 14:49:51 +0800

    1

--- resources/views/AnimalShelter/Subscription/Paypal/subscribe.blade.php
+++ resources/views/AnimalShelter/Subscription/Paypal/subscribe.blade.php
@@ -2,12 +2,6 @@
 @push('css')
 <link href="{{url('css/style.css')}}" rel="stylesheet">    
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.css" integrity="sha512-jU/7UFiaW5UBGODEopEqnbIAHOI8fO6T99m7Tsmqs2gkdujByJfkCbbfPSN4Wlqlb9TGnsuC0YgUgWkRBK7B9A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-<link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <style>
             /* The Modal (background) */
             .modal-confirm {		
