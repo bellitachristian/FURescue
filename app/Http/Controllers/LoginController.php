d0d7400eef8b543e9fa32579b7672326ac1dabc4
bellitachristian <bellitachristian78u@gmail.com>
2021-12-07 23:17:28 +0800

    1

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -88,7 +88,7 @@ class LoginController extends Controller
                                     'LoggedUserInfo'=>AnimalShelter::where('id','=',session('LoggedUser'))->first(),
                                     'shelter'=>AnimalShelter::where('id','=',session('LoggedUser'))->first()
                                 );
-                               return redirect()->route('viewpage.reactivation');  //adtu sa view for reactivation of account
+                               return redirect()->route('page.reactivation');  //adtu sa view for reactivation of account
                             }
                         }
                         else{
