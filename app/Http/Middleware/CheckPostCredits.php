5b5627f0d599ee069a026677b01004b567be5aaa
bellitachristian <bellitachristian78u@gmail.com>
2021-12-11 11:19:20 +0800

    1

--- app/Http/Middleware/CheckPostCredits.php
+++ app/Http/Middleware/CheckPostCredits.php
@@ -23,8 +23,6 @@ class CheckPostCredits
         if($shelter->TotalCredits == "0" && $shelter->TotalCredits != "UNLI"){ 
             return back()->with('status','You have 0 post credit, subscribe to the following promos in order to get full control of the system.');
         }   
-        return $next($request)->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
-                              ->header('Pragma','no-cache')
-                              ->header('Expires','Sat, 26 Jul 1997 05:00:00 GMT');
+        return $next($request);
     }
 }
