3e97966d338f9dc7729364008c4a5189271c2b5b
bellitachristian <bellitachristian78u@gmail.com>
2021-12-11 13:16:40 +0800

    1

--- app/Http/Controllers/PetOwnerManagement.php
+++ app/Http/Controllers/PetOwnerManagement.php
@@ -2174,7 +2174,10 @@ class PetOwnerManagement extends Controller
                         whereHas('petownerPhoto',function($query)use($petowner){
                           $query->where('petowner_id',$petowner->id);
                         })
-                        ->where('is_verified_petowner','2')
+                        -> where(function($query1){
+                            $query1-> where('is_verified_petowner', '2')
+                                    ->orWhere('is_verified_petowner','1');
+                        })
                         ->where('grace','!=','0')
                         ->count();
         $data =array(
