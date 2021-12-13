1cc25b884d021633eb1670314f9e8074559a3f1f
bellitachristian <bellitachristian78u@gmail.com>
2021-12-13 17:27:36 +0800

    1

--- resources/views/AnimalShelter/welcomepage.blade.php
+++ resources/views/AnimalShelter/welcomepage.blade.php
@@ -1,6 +1,7 @@
 <html>
   <head>
     <title>FURESCUE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style type="text/css">
     *{
       margin: 0;
@@ -9,64 +10,86 @@
     }
     
     html{
-      background: url("images/background.jpg");
-      background-repeat: no-repeat;
-      background-size: 100% 100%;
-      background-position: top center; 
-   
+      background: url("background.jpg") no-repeat center fixed;
+      background-size: cover;
     }
-    
-    a{
-      text-decoration: none;
+
+    .welcome{
+      padding-top: 100px;
+      font-weight: 700;
+      font-family: Arial;
+      font-style: bold;
       color: white;
-      border: none;
-      padding: 15px 32px;
-      text-align: center;
-      display: inline-block;
-      font-size: 16px;
-      background-color: #1D630F;
+      font-size: 50px;
+      max-width: 31%;
     }
 
-    img{
-      width: 200px;
-      padding: 5px;
-    }
-
-    #welcome{
-      color: white;
-      font-size: 30px;
-    }
-
-    #welcome1{
+    .welcome1{
       color: white;
       font-size: 15px; 
-    }
-    .slides {
-      margin-left: 50px;
+      max-width: 31%;
+      text-align: justify;
     }
 
-    .caption-container {
+    .caption-container { 
       color: black;
       font-family: Arial; 
       font-size: 20px;
-      float: right;
-      margin-top: 3%;
+      margin-left: 50px;
       margin-right: 50px;
     }
 
-    #smlpic{
-      float:  right;
-      margin-top: 170px;
+    .button{
+      margin-left: 30px;
+      font-size: 15px;
+      padding: 13px;
+      text-align: center;
+      transition: 0.5s;
+      background-size: 200% auto;
+      color: white;
+      text-decoration: none;
+      box-shadow: 0 0 20px #eee;
+      box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+      transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+      cursor: pointer;
+      display: inline-block;
+      border-radius: 20px;
+      background-image: linear-gradient(to right, #1D630F 0%, #167C02 51%, #000000 100%)
     }
+
+    .button1{
+      font-size: 15px;
+      font-family: arial;
+      color: black;
+      float: right;
+      padding: 20px;
+      text-align: center;
+      transition: 0.5s;
+      background-size: 200% auto;
+      text-decoration: none;   
+      margin-right: 10px; 
+    }
+    
+    .button:hover{
+      box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+      margin: 8px 10px 12px;
+      background-position: right center;
+    }
+
+    .button1:hover{
+      font-size: 15px;
+      padding: 20px;
+      background-color: #1D630F;
+      color: white;
+    }
+
     </style> 
   </head>
   <body>
-    <div class="logo"><img src ="{{asset('images/final-logo-part2.png')}}"/><div>
-        <div class="caption-container">
-            <p id="welcome">WELCOME TO FURESCUE</p>
-            <p id="welcome1"><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FURescue, would hasten up the adoption process guided<br> by a strategy called WIN-BUILD-ADOPT.  </p><br><br>
-            <p><a href="/dashboard">LET'S START SAVING A LIFE WITH FURESCUE &nbsp ></a></p>
-            <div id="smlpic"><img src = "{{asset('images/bg-pet5-E.png')}}"/></div>
-        </div>
+      <div class="caption-container">
+          <p class="welcome">WELCOME TO FURESCUE</p>
+          <p class="welcome1"><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FURescue, which stands for Fauna Urgent Rescue, would hasten up the adoption process guided by a strategy called WIN-BUILD-ADOPT. WIN – win the hearts of possible owners by showing relevant information of a pet. BUILD – build trust with possible adopters who are able to comply with the adoption requirements. ADOPT – family love achieved.   </p><br><br>
+          <a class="button" href = "/dashboard">Start Saving A Life With FURescue</a>
+      </div>
   </body>
 </html>
